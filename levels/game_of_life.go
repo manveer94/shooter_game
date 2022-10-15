2c01480101fb85572277def8d5ee9f944bef8a7d
manveer94 <manveer.1694.dev@gmail.com>
2022-10-15 14:02:15 +0530

    created grid

--- levels/game_of_life.go
+++ levels/game_of_life.go
@@ -2,6 +2,7 @@ package levels
 
 import (
 	"github.com/veandco/go-sdl2/sdl"
+	"manveer/exp/components/gol"
 )
 
 func CreateGameOfLife(renderer *sdl.Renderer, window *sdl.Window) *Level {
@@ -10,20 +11,54 @@ func CreateGameOfLife(renderer *sdl.Renderer, window *sdl.Window) *Level {
 	level.renderer = renderer
 	level.window = window
 
-	level.onStart = func(renderer *sdl.Renderer) {
+	level.onStart = func(renderer *sdl.Renderer) error {
 		window.SetSize(1000, 1000)
 		renderer.SetDrawColor(255, 255, 255, 255)
 		renderer.Clear()
 
-		renderer.SetDrawColor(0, 0, 0, 255)
-		renderer.DrawLine(10, 10, 100, 100)
-		//renderer.Clear()
+		err := gol.CreateGrid(renderer)
+		if err != nil {
+			return err
+		}
 
 		renderer.Present()
+		return nil
 	}
 
-	level.onFrameChange = func(renderer *sdl.Renderer) {
+	//isPressed := false
 
+	level.onFrameChange = func(renderer *sdl.Renderer) error {
+
+		//x, y, state := sdl.GetMouseState()
+		////fmt.Printf("%d, %d, %v\n", x, y, state)
+		//
+		//if state&sdl.ButtonLMask() != 0 {
+		//	renderer.SetDrawColor(255, 0, 0, 255)
+		//	isPressed = true
+		//}
+		//
+		//if state&sdl.ButtonMMask() != 0 {
+		//	renderer.SetDrawColor(0, 255, 0, 255)
+		//	isPressed = true
+		//}
+		//
+		//if state&sdl.ButtonRMask() != 0 {
+		//	renderer.SetDrawColor(0, 0, 255, 255)
+		//	isPressed = true
+		//}
+		//
+		//if state == sdl.RELEASED && isPressed {
+		//	isPressed = false
+		//
+		//	fmt.Printf("%d, %d, %v\n", x, y, state&sdl.ButtonLMask())
+		//	renderer.FillRect(&sdl.Rect{
+		//		X: x, Y: y,
+		//		W: 50, H: 50,
+		//	})
+		//
+		//	renderer.Present()
+		//}
+		return nil
 	}
 
 	return level
